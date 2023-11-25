5daa3a0f6acb5d317997813a4637f04d8159282d
ddatta <deb.datta@gmail.com>
2023-11-25 01:45:57 +0100

    init commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function DashboardPage() {
+  return (
+    <div>
+        <h1>Dashboard Page</h1>
+        <Button variant="destructive">Click Me</Button>
+    </div>
+  );
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function LandingPage() {
+  return (
+    <div>
+        Landing Page (Unprotected)
+    </div>
+  );
+}
